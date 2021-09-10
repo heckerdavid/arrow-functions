f9743456cce4bca28705e9d6b9c9625669a960c4
David Hecker <HECKERDW@GMAIL.COM>
2021-09-10 16:21:00 -0700

    Completed Q3.

--- starter-code/app.js
+++ starter-code/app.js
@@ -121,14 +121,14 @@ let objectLit = () => ({key1: 'value1', key2: 'value2', key3: 'value3',})
 console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
+let sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
 let message = function(name) {
